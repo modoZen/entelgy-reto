5d90cc2e1549a26fd302995dfbd0799d6eda219e
Max <mdiazn@uni.pe>
2021-08-25 01:50:30 -0500

    correciones sobre el servicio

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+async function xd() {
+    let data = await getData();
+    console.log(data[0])
+}
+xd();
--- services/index.js
+++ services/index.js
@@ -3,13 +3,10 @@ const API = 'https://restcountries.eu/rest/v2/lang/es';
 const getData = async (id='') =>{
     const apiUrl = id!=''?`${API}${id}` : API;
     try {
-        const data = await fetch(apiUrl);
+        const response = await fetch(apiUrl);
+        const data = await response.json();
         return data
     } catch (e) {
         console.log('Fetch Error', error);
     }
 }
-
-export default {
-    getData
-};
