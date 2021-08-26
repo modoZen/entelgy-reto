895512a1af6462b2d38d675b3cc9b5b486de0979
Max <mdiazn@uni.pe>
2021-08-25 22:21:19 -0500

    prueba de renderizado de datos de API

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-
-
-async function xd() {
-    let data = await getData();
-    console.log(data[0])
+async function App() {
+    try {
+        const data = await getData();
+        console.log(data)
+        console.log(main_container);
+        main_container.innerHTML = data.map(country=>`
+        <div class="main_card">
+            <div class="pH">Pais: ${country.name}</div>
+            <div>Capital: ${country.capital}</div>
+            <div>Población: ${(country.population).toLocaleString('en').replace(/,/g,' ')}</div>
+        </div>
+        `).join('');
+    } catch (e) {
+        console.error('Error en el App',e.message)
+    }
 }
-xd();
+const main_container = document.getElementById('main_container');
+App();
